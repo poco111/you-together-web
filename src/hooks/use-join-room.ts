e92e8cb26a02eccc656bdbad707806e6bb5fb135
poco <godiva140@gmail.com>
2024-05-07 19:35:54 +0900

    Refactor: 토큰 -> 쿠키 인증 방법 수정으로 react query 수정

--- src/hooks/use-join-room.ts
+++ src/hooks/use-join-room.ts
@@ -1,16 +1,14 @@
 import { joinRoom } from '@/api/join-room';
 import paths from '@/paths';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
 const useJoinRoom = () => {
   const router = useRouter();
-  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: ({ roomCode }: { roomCode: string }) => joinRoom({ roomCode }),
     onSuccess: (response) => {
-      queryClient.setQueryData(['token'], response.headers['authorization']);
       router.push(paths.room(response.data.data.roomCode));
     },
   });
