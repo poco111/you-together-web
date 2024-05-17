0b13ad08a08d9654f508c42096b6eb423af1a6a1
poco <godiva140@gmail.com>
2024-05-18 02:11:09 +0900

    Refactor: 사용자 역할 변경 드롭다운 로직 수정

---
+++ src/service/user-action.ts
@@ -0,0 +1,42 @@
+type TDropdownContents = {
+  contentsType: 'NICK_NAME' | 'CHANGE_ROLE' | 'NONE';
+  dropdownContents: string[];
+};
+
+export const getDropdownContents = (
+  userInfo: TUserInfo,
+  targetUserInfo: TUserInfo
+): TDropdownContents => {
+  const { userId: curUserId, role: curUserRole } = userInfo;
+  const { userId: targetUserId, role: targetUserRole } = targetUserInfo;
+
+  if (curUserId === targetUserId) {
+    return { contentsType: 'NICK_NAME', dropdownContents: [] };
+  }
+
+  const userRoles = ['HOST', 'MANAGER', 'EDITOR', 'GUEST', 'VIEWER'];
+  const userRatings = new Map();
+  userRoles.forEach((role, index) => {
+    userRatings.set(role, index);
+  });
+
+  if (
+    curUserRole === 'EDITOR' ||
+    curUserRole === 'GUEST' ||
+    curUserRole === 'VIEWER' ||
+    userRatings.get(curUserRole) >= userRatings.get(targetUserRole)
+  ) {
+    return { contentsType: 'NONE', dropdownContents: [] };
+  }
+
+  const dropdownContents: string[] = [];
+  const userRating = userRatings.get(userInfo?.role);
+  const targetUserRating = userRatings.get(targetUserInfo?.role);
+  userRatings.forEach((rating, role) => {
+    if (userRating <= rating && rating < targetUserRating) {
+      dropdownContents.push(role);
+    }
+  });
+
+  return { contentsType: 'CHANGE_ROLE', dropdownContents: dropdownContents };
+};
