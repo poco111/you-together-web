7a422f0cb69facd07ce04ce2b2a96b3ae094d6e1
poco <godiva140@gmail.com>
2024-05-24 20:52:15 +0900

    Feat: Icon 적용 및 채팅방 스타일 수정

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -19,6 +19,13 @@ import {
   Dropdown,
   DropdownMenu,
   DropdownItem,
+  Image,
+  Listbox,
+  ListboxItem,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
   useDisclosure,
 } from '@nextui-org/react';
 import { useState } from 'react';
@@ -26,10 +33,14 @@ import { useRouter } from 'next/navigation';
 import paths from '@/paths';
 import useGetUserInfo from '@/hooks/use-user-info';
 import useChangeRole from '@/hooks/use-change-role';
+import useGetPlaylist from '@/hooks/use-playlist';
 import {
   getDropdownContents,
   getNicknameFromUserId,
 } from '@/service/user-action';
+import Link from 'next/link';
+import Icon from '@/assets/icon';
+// import PlayList from '@/components/playlist';
 
 const RoomPage = ({ params: { id } }: { params: { id: string } }) => {
   const roomCode = id;
@@ -37,9 +48,12 @@ const RoomPage = ({ params: { id } }: { params: { id: string } }) => {
   const { data: chats = [] } = useChatMessage({ roomCode });
   const { data: participants = [] } = useGetParticipants({ roomCode });
   const { data: userInfo } = useGetUserInfo({ roomCode });
+  const { data: playlist = [] } = useGetPlaylist({ roomCode });
   const { mutate: changeUserRole } = useChangeRole();
   const [chatValue, setChatValue] = useState('');
   const participantsList = participants?.[0]?.participants;
+  const playlistInfo = playlist?.[0]?.playlist;
+
   const router = useRouter();
   const {
     isOpen: isChangeNicknameModalOpen,
@@ -113,84 +127,114 @@ const RoomPage = ({ params: { id } }: { params: { id: string } }) => {
   };
 
   return (
-    <div>
-      <ScrollShadow hideScrollBar className="w-96 h-96">
-        {chats.map((chat) => {
-          return chat.messageType === 'CHAT' ? (
-            <div key={chat.chatId}>
-              {`[${
-                getNicknameFromUserId(chat.userId, participantsList) ??
-                '알수없음'
-              }]`}{' '}
-              : {chat.content} {chat.createdAt}
-            </div>
-          ) : (
-            <div key={chat.chatId}>[알림] {chat.content}</div>
-          );
-        })}
-      </ScrollShadow>
+    <>
+      <Navbar className="shadow-gray-800 shadow-lg m-0">
+        <NavbarBrand>
+          <Link href={paths.home()} className="font-bold text-2xl">
+            <span className="text-red-500">Y</span>ou
+            <span className="text-red-500">T</span>ogether
+          </Link>
+        </NavbarBrand>
+        <NavbarContent justify="center" />
+        <NavbarContent className="flex" justify="end">
+          <NavbarItem>
+            <div>방정보</div>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
 
-      <Textarea
-        label="chat"
-        placeholder="채팅을 입력하세요"
-        value={chatValue}
-        onChange={(e) => setChatValue(e.target.value)}
-      />
-      <button
-        className="rounded-md border-slate-50 border-solid border-2"
-        onClick={() => handleSendChat(chatValue)}
-      >
-        채팅 보내기
-      </button>
+      <div className="flex justify-center items-center px-40">
+        <div className="x-fit">
+          <div>hi</div>
+        </div>
+        <div className="w-96">
+          <ScrollShadow hideScrollBar className="w-96 h-96 mb-3">
+            {chats.map((chat) => {
+              return chat.messageType === 'CHAT' ? (
+                <div key={chat.chatId} className="inline-block gap-2 text-xs">
+                  <span className="text-gray-500">
+                    {`${
+                      getNicknameFromUserId(chat.userId, participantsList) ??
+                      '알수없음'
+                    }`}{' '}
+                  </span>
+                  <span>
+                    {chat.content} {chat.createdAt}
+                  </span>
+                </div>
+              ) : (
+                <div key={chat.chatId} className="text-xs text-amber-200">
+                  [알림] {chat.content}
+                </div>
+              );
+            })}
+          </ScrollShadow>
 
-      <Table isStriped aria-label="Participants table" className="w-80 h-64">
-        <TableHeader className="sticky top-32">
-          <TableColumn>NAME</TableColumn>
-          <TableColumn>ROLE</TableColumn>
-          <TableColumn>Action</TableColumn>
-        </TableHeader>
-        <TableBody>
-          {participantsList?.map((participant) => {
-            const isDisabled =
-              userInfo &&
-              getDropdownContents(userInfo, participant).contentsType ===
-                'NONE';
-            return (
-              <TableRow key={participant.userId}>
-                <TableCell>{participant.nickname}</TableCell>
-                <TableCell>{participant.role}</TableCell>
-                <TableCell>
-                  <Dropdown>
-                    <DropdownTrigger>
-                      <Button
-                        isIconOnly
-                        size="sm"
-                        variant="light"
-                        disabled={isDisabled}
-                      >
-                        아이콘
-                      </Button>
-                    </DropdownTrigger>
-                    {!isDisabled &&
-                      userInfo &&
-                      renderDropdownContent(userInfo, participant)}
-                  </Dropdown>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+          <div className="flex gap-3 h-16">
+            <Textarea
+              placeholder="채팅을 입력하세요"
+              value={chatValue}
+              onChange={(e) => setChatValue(e.target.value)}
+            />
+            <button onClick={() => handleSendChat(chatValue)}>
+              <Icon name="sendMessage" className="w-5 h-5" />
+            </button>
+          </div>
+          <Table
+            isStriped
+            aria-label="Participants table"
+            className="min-x-auto min-h-auto max-h-64 overflow-auto"
+          >
+            <TableHeader className="sticky top-32">
+              <TableColumn>NAME</TableColumn>
+              <TableColumn>ROLE</TableColumn>
+              <TableColumn>Action</TableColumn>
+            </TableHeader>
+            <TableBody>
+              {participantsList?.map((participant) => {
+                const isDisabled =
+                  userInfo &&
+                  getDropdownContents(userInfo, participant).contentsType ===
+                    'NONE';
+                return (
+                  <TableRow key={participant.userId}>
+                    <TableCell>{participant.nickname}</TableCell>
+                    <TableCell>{participant.role}</TableCell>
+                    <TableCell>
+                      <Dropdown>
+                        <DropdownTrigger>
+                          <Button
+                            isIconOnly
+                            size="sm"
+                            variant="light"
+                            disabled={isDisabled}
+                            className="pl-1"
+                          >
+                            <Icon name="ellipsis" className="size-5" />
+                          </Button>
+                        </DropdownTrigger>
+                        {!isDisabled &&
+                          userInfo &&
+                          renderDropdownContent(userInfo, participant)}
+                      </Dropdown>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
 
-      {!!isChangeNicknameModalOpen && (
-        <ChangeNicknameModal
-          isOpen={isChangeNicknameModalOpen}
-          onOpenChange={onChangeNicknameModalOpenChange}
-          onClose={onChangeNicknameModalClose}
-          roomCode={roomCode}
-        />
-      )}
-    </div>
+          {!!isChangeNicknameModalOpen && (
+            <ChangeNicknameModal
+              isOpen={isChangeNicknameModalOpen}
+              onOpenChange={onChangeNicknameModalOpenChange}
+              onClose={onChangeNicknameModalClose}
+              roomCode={roomCode}
+            />
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 export default RoomPage;
